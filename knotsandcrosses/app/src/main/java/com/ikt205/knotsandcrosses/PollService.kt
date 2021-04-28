e08d358bb7ce1226e62953e632aadc80c26b792a
Simon Fladen Lie <simonfl@uia.no>
2021-04-28 15:02:20 +0200

    Added turn based functionality

--- knotsandcrosses/app/src/main/java/com/ikt205/knotsandcrosses/PollService.kt
+++ knotsandcrosses/app/src/main/java/com/ikt205/knotsandcrosses/PollService.kt
@@ -4,7 +4,10 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.os.Looper
+import android.widget.Button
 import com.ikt205.knotsandcrosses.GameManager.pollState
+import com.ikt205.knotsandcrosses.GameManager.contendersTurn
 import kotlinx.coroutines.*
 
 class PollService : Service() {
@@ -16,22 +19,29 @@ class PollService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+
         GlobalScope.launch {
+
             while (true) {
                 // launch a new coroutine in background and continue
-                delay(5000)
-                val tempList = pollState
-                GameManager.pollGame(applicationContext)
-                delay(1000)
 
-                if (tempList.toString() != pollState.toString()){
-                    print("to string fungerer")
-                }
+                while (contendersTurn) {
+                    delay(5000)
+                    var tempList = pollState
 
-                if(!(tempList.containsAll(pollState) && pollState.containsAll(tempList))){
-                    println("listen er ulik")
+                    GameManager.pollGame(applicationContext)
+                    delay(1000)
+
+                    if (!(tempList.containsAll(pollState) && pollState.containsAll(tempList))) {
+                        println("Min tur")
+                        contendersTurn = false
+
+                    } else {
+                        //GameActivity().turnBased()
+                        println("Motstanders trekk")
+                    }
                 }
-//                println("new pollstate:"+ pollState)
             }
         }
 
