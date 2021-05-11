38de534fa706ec80c71db52d32ee2efe7c14c0e9
Simon Fladen Lie <simonfl@uia.no>
2021-05-11 09:55:31 +0200

    Added testing

--- knotsandcrosses/app/src/main/java/com/ikt205/knotsandcrosses/GameState.kt
+++ knotsandcrosses/app/src/main/java/com/ikt205/knotsandcrosses/GameState.kt
@@ -1,10 +1,21 @@
 package com.ikt205.knotsandcrosses
 
 import android.os.Parcelable
+import com.ikt205.knotsandcrosses.GameManager.gameList
 import kotlinx.android.parcel.Parcelize
 
 typealias GameState = List<List<Int>>
 
 @Parcelize
-data class Game(val players:MutableList<String>, val gameId:String, val state: String):Parcelable
+data class Game(val players:MutableList<String>, val gameId:String, val state: String):Parcelable {
+    fun numbersOfPlayers(): Int {
+        return players.size
+    }
+
+    fun tilesTaken(): Int {
+        return gameList.size
+    }
+
+
+}
 
